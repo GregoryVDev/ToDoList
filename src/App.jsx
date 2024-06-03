b32b72104e9f44e9f7a03a5c2b53270aea0c7a1c
GregoryVDev <arexo@hotmail.fr>
2024-06-03 10:19:51 +0200

    Added the function Counter with two buttons (incrémenter, décrémenter) and import App.css in ToggleButton.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@
 import { HelloWorld } from "./HelloWorld";
 import { Paragraphe } from "./Paragraphe";
 import { ByeWorld, Description, Titreh3 } from "./ByeWorld";
-import { ToggleButton } from "./ToggleButton";
+import { ToggleButton, Counter } from "./ToggleButton";
 
 export const App = () => {
   return (
@@ -34,6 +34,7 @@ export const App = () => {
       <Titreh3 />
       <Description descripte="Je décris ce que je fais, là j'ai fais plusieurs composants pour apprendre, puis on pratique, mieux on apprend" />
       <ToggleButton />
+      <Counter />
     </div>
   );
 };
--- src/ToggleButton.jsx
+++ src/ToggleButton.jsx
@@ -1,7 +1,24 @@
+import "./App.css";
 import React, { useState } from "react";
 
 export function ToggleButton() {
   const [isOn, setIsOn] = useState(false);
 
-  return <button onClick={() => setIsOn(!isOn)}>{isOn ? "ON" : "OFF"}</button>;
+  return (
+    <button onClick={() => setIsOn(!isOn)} className="button-off-on">
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
+}
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Incrémenter</button>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count - 1)}>Décrémenter</button>
+    </div>
+  );
 }
